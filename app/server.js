f5c369b7fbfd6cbdb4896b53a3288ecfac56f71a
gh2o <geomercorepro@outlook.fr>
2023-06-23 00:46:28 +0200

    Backend

---
+++ app/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware pour parser le corps des requêtes en JSON
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur l\'API de mon application !');
+});
+
+// Lancement du serveur
+app.listen(port, () => {
+    console.log(`Serveur en cours d'exécution sur le port ${port}`);
+});
